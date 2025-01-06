d484a6b629ce1701841e47f654b9b265d3785c2b
Alshershaby <elshershaby93@gmail.com>
2025-01-07 00:12:26 +0200

    fixing image and link routing

--- src/app/become-partner-section/become-partner.component.ts
+++ src/app/become-partner-section/become-partner.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-
+import { RouterLink, RouterLinkActive, RouterModule } from '@angular/router';
 @Component({
   selector: 'app-become-partner',
-  imports: [],
+  imports: [RouterLink,RouterLinkActive,RouterModule],
   templateUrl: './become-partner.component.html',
   styleUrl: './become-partner.component.scss'
 })
