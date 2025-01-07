ebee55ff3e87eaa4c715a9dd1ad1fb8f7d7e160b
Alshershaby <elshershaby93@gmail.com>
2025-01-07 12:44:20 +0200

    images fixxing

--- src/app/jet/jet.component.ts
+++ src/app/jet/jet.component.ts
@@ -1,13 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
+import {FormControl,FormGroup,ReactiveFormsModule,Validators} from '@angular/forms';
+
 @Component({
   selector: 'app-jet',
-  imports: [CommonModule],
+  imports: [CommonModule,ReactiveFormsModule],
   templateUrl: './jet.component.html',
   styleUrl: './jet.component.scss'
 })
 export class JetComponent {
+
+// form data 
+jetForm:FormGroup= new FormGroup({
+  
+})
+
+
+// hover elements 
   hoveredIndex: number | null = null;
 
   onMouseOver(index: number) {
