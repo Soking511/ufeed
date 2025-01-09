fc6688d634a2a5987f399d14f3a7b3aa28ed873d
Alshershaby <elshershaby93@gmail.com>
2025-01-10 01:38:27 +0200

    adding forms with validation

--- src/app/ees/ees.component.ts
+++ src/app/ees/ees.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import {FormControl,FormGroup,ReactiveFormsModule,Validators} from '@angular/forms';
+
 @Component({
   selector: 'app-ees',
-  imports: [CommonModule],
+  imports: [CommonModule,ReactiveFormsModule],
   templateUrl: './ees.component.html',
   styleUrl: './ees.component.scss'
 })
@@ -17,4 +19,33 @@ export class EesComponent {
   onMouseOut() {
     this.hoveredIndex = null; // Reset the hover effect
   }
+
+
+  // ----------
+  // form data 
+
+
+  eesForm:FormGroup= new FormGroup({
+    username:new FormControl(null,[ Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(20),
+      Validators.pattern(/^(?!.*[_.]{2})[a-zA-Z._]{3,20}$/)
+    ]),
+    contactNumber:new FormControl(null, [
+      Validators.required,
+      Validators.pattern(/^(?:\+?\d{1,4}[\s-]?)?(?:\(?\d{1,4}\)?[\s-]?)?\d{7,10}$/)
+    ]),
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    companyName: new FormControl(null, [ Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(20),
+      Validators.pattern(/^(?!.*[_.]{2})[a-zA-Z._]{3,20}$/)
+    ]),
+    NumberofEmployees: new FormControl(null, [
+      Validators.required,
+      Validators.min(1),    // Set minimum value to 1
+      Validators.max(12000)    // Set maximum value to 100
+    ])
+  
+  })
 }
--- src/app/jet/jet.component.ts
+++ src/app/jet/jet.component.ts
@@ -13,7 +13,27 @@ export class JetComponent {
 
 // form data 
 jetForm:FormGroup= new FormGroup({
-  
+  username:new FormControl(null,[ Validators.required,
+    Validators.minLength(3),
+    Validators.maxLength(20),
+    Validators.pattern(/^(?!.*[_.]{2})[a-zA-Z._]{3,20}$/)
+  ]),
+  contactNumber:new FormControl(null, [
+    Validators.required,
+    Validators.pattern(/^(?:\+?\d{1,4}[\s-]?)?(?:\(?\d{1,4}\)?[\s-]?)?\d{7,10}$/)
+  ]),
+  email: new FormControl(null, [Validators.required, Validators.email]),
+  companyName: new FormControl(null, [ Validators.required,
+    Validators.minLength(3),
+    Validators.maxLength(20),
+    Validators.pattern(/^(?!.*[_.]{2})[a-zA-Z._]{3,20}$/)
+  ]),
+  companydepartmentNum: new FormControl(null, [
+    Validators.required,
+    Validators.min(1),    // Set minimum value to 1
+    Validators.max(100)    // Set maximum value to 100
+  ])
+
 })
 
 
