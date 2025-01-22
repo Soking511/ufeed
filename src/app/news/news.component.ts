f5ef137d269d6d786828637ae1d3d28a4020855f
Alshershaby <elshershaby93@gmail.com>
2025-01-22 16:35:58 +0200

    fixing mob version

--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-news',
-  imports: [],
+  imports: [CommonModule,RouterModule],
   templateUrl: './news.component.html',
   styleUrl: './news.component.scss'
 })
