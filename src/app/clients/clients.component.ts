1a216e6e6298eed26db7c45a45f1a9387f16746e
Alshershaby <elshershaby93@gmail.com>
2025-01-22 06:14:33 +0200

    fixing slider

--- src/app/clients/clients.component.ts
+++ src/app/clients/clients.component.ts
@@ -1,11 +1,45 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common'; 
 @Component({
   selector: 'app-clients',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './clients.component.html',
   styleUrl: './clients.component.scss'
 })
 export class ClientsComponent {
+  images = [
+    'public/clients/1.png',
+    'public/clients/2.png',
+    'public/clients/3.png',
+    'public/clients/4.png',
+    'public/clients/5.png',
+    'public/clients/6.png',
+    'public/clients/7.png',
+    'public/clients/8.png',
+    'public/clients/9.png',
+    'public/clients/10.png'
+  ];
+  currentOffset = 0;
+  visibleItems = 5;
+  itemWidth = 100 / this.visibleItems; // 100% divided by number of visible items (5)
 
+  ngOnInit() {
+    // Auto slide every 3 seconds
+    setInterval(() => this.slideRight(), 4000);
+  }
+
+  slideLeft() {
+    if (this.currentOffset < 0) {
+      this.currentOffset += this.itemWidth;
+    }
+  }
+
+  slideRight() {
+    const maxOffset = -(this.images.length - this.visibleItems) * this.itemWidth;
+    if (this.currentOffset > maxOffset) {
+      this.currentOffset -= this.itemWidth;
+    } else {
+      this.currentOffset = 0; // Loop back to the beginning
+    }
+  }
 }
