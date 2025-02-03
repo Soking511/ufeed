02da11b360060a3d1c0af35eb17d2091dcb585d8
Alshershaby <elshershaby93@gmail.com>
2025-02-03 12:50:53 +0200

    fixing comment

--- src/app/become-apartner/become-apartner.component.ts
+++ src/app/become-apartner/become-apartner.component.ts
@@ -11,6 +11,8 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./become-apartner.component.scss']
 })
 export class BecomeAPartnerComponent {
+  isOtherSelected: boolean = false;
+
   becomePartner: FormGroup = new FormGroup({
     companyName: new FormControl(null, [
       Validators.required,
@@ -24,8 +26,9 @@ export class BecomeAPartnerComponent {
       Validators.required,
       Validators.pattern(/^(?:\+?\d{1,4}[\s-]?)?(?:\(?\d{1,4}\)?[\s-]?)?\d{7,10}$/)
     ]),
-    PartnerType: new FormControl(null,Validators.required),
-    country: new FormControl(null,Validators.required),
+    PartnerType: new FormControl(null, Validators.required),
+    OtherPartnerType: new FormControl(''), // Additional input field for 'Other'
+    country: new FormControl(null, Validators.required),
     personalProfile: new FormControl(),
     personalName: new FormControl(null, [
       Validators.required,
@@ -42,11 +45,22 @@ export class BecomeAPartnerComponent {
     message: new FormControl()
   });
 
-
-
-  
   // Inject HttpClient
   constructor(private http: HttpClient) {}
+
+  // Function to handle selection change
+  onPartnerTypeChange(event: Event) {
+    const selectedValue = (event.target as HTMLSelectElement).value;
+    this.isOtherSelected = selectedValue === 'Other';
+
+    if (this.isOtherSelected) {
+      this.becomePartner.get('OtherPartnerType')?.setValidators([Validators.required]);
+    } else {
+      this.becomePartner.get('OtherPartnerType')?.clearValidators();
+      this.becomePartner.get('OtherPartnerType')?.setValue('');
+    }
+    this.becomePartner.get('OtherPartnerType')?.updateValueAndValidity();
+  }
 
   // Function to handle form submission
   onSubmit() {
