aaedf41768ef6f07fd67844342aed06fa449ba8a
Alshershaby <elshershaby93@gmail.com>
2025-02-09 13:39:52 +0200

    comments fixed

--- src/app/ngo/ngo.component.ts
+++ src/app/ngo/ngo.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import {FormControl,FormGroup,ReactiveFormsModule,Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-ngo',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './ngo.component.html',
   styleUrl: './ngo.component.scss'
 })
 export class NgoComponent {
+
+  contactNgo:FormGroup = new FormGroup({
+    
+  })
 
 }
