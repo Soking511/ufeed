7cc3afcae7d885c4cfd7f81e79f24f4ed5c219d4
MohamedAshraf0-0 <mohamed.a100200@icloud.com>
2025-03-12 22:30:37 +0200

    .

--- README.md
+++ README.md
@@ -57,3 +57,11 @@ Angular CLI does not come with an end-to-end testing framework by default. You c
 ## Additional Resources
 
 For more information on using the Angular CLI, including detailed command references, visit the [Angular CLI Overview and Command Reference](https://angular.dev/tools/cli) page.
+ 
+## update the github page
+
+# Build the Angular project with the correct base-href
+ng build --configuration production --base-href "/ufeed/"
+
+# Deploy to GitHub Pages
+npx angular-cli-ghpages --dir=docs/browser
--- src/app/clients/clients.component.ts
+++ src/app/clients/clients.component.ts
@@ -1,11 +1,11 @@
 import { Component, HostListener, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common'; 
+import { CommonModule } from '@angular/common';
 import { TranslationService } from '../services/translation.service';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-clients',
-  imports: [CommonModule,TranslateModule],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './clients.component.html',
   styleUrl: './clients.component.scss'
 })
@@ -23,6 +23,18 @@ export class ClientsComponent implements OnInit {
     'public/clients/9.png',
     'public/clients/10.png',
   ];
+  clients = [
+    { id: '1', link: 'https://www.egytrans.com/home', logo: 'public/clients/1.png' },
+    { id: '2', link: 'https://www.ykk.com/english/', logo: 'public/clients/2.png' },
+    { id: '3', link: 'https://www.lamaregypt.com/en', logo: 'public/clients/3.png' },
+    { id: '4', link: 'https://eg.linkedin.com/company/digital-egypt-for-investment-co?original_referer=https%3A%2F%2Fwww.google.com%2F', logo: 'public/clients/4.png' },
+    { id: '5', link: 'https://mediterraneo-egypt.com/', logo: 'public/clients/5.png' },
+    { id: '6', link: 'https://eventumsolutions.com/', logo: 'public/clients/6.png' },
+    { id: '7', link: 'https://www.heidelbergmaterials.com/en', logo: 'public/clients/7.png' },
+    { id: '8', link: 'https://alnafitha.com/ar/', logo: 'public/clients/8.png' },
+    { id: '9', link: 'https://eg.linkedin.com/company/alsharkeya-sugar', logo: 'public/clients/9.png' },
+    { id: '10', link: 'https://www.epp-eg.com/', logo: 'public/clients/10.png' },
+  ]
   currentOffset = 0;
   visibleItems = 5;
   itemWidth = 100 / this.visibleItems;
